988ef554e41a130cf32786909910cffc42733354
Jaume Singla Valls <singlavalls@mediamarktsaturn.com>
2020-05-07 17:44:12 +0200

    updated urls

--- monit_collector/monitcollector/templatetags/monitcollector_tags.py
+++ monit_collector/monitcollector/templatetags/monitcollector_tags.py
@@ -11,11 +11,13 @@ try:
 except:
     monit_update_period = 60
 
+
 @register.filter
 def timestamp_to_date(timestamp):
     if not isinstance(timestamp, int):
         return ""
     return timezone.datetime.fromtimestamp(timestamp)
+
 
 @register.filter
 def time_class(timestamp):
@@ -24,6 +26,7 @@ def time_class(timestamp):
     if int(time.time()) > int(timestamp) + 3*monit_update_period:
         return "danger"
     return ""
+
 
 @register.filter
 def time_str(uptime):
@@ -35,25 +38,26 @@ def time_str(uptime):
     days = (uptime/24/60/60) % 365
     years = uptime/365/24/60/60
     if years == 0:
-      if days == 0:
-        if hours == 0:
-          return "%sm" % mins
-        return "%sh %sm" % (hours, mins)
-      return "%sd %sh %sm" % (days, hours, mins)
-    return "%sy %sd %sh %sm" % (years, days, hours, mins)
+        if days == 0:
+            if hours == 0:
+                return "{:.2}m".format(mins)
+            return "{:.2}h {:.2}m".format(hours, mins)
+        return "{:.2}d {:.2}h {:.2}m".format(days, hours, mins)
+    return "{:.2}y {:.2}d {:.2}h {:.2}m".format(years, days, hours, mins)
 
 # does nothing at the moment!
 @register.filter
 def status_str(status, monitor):
     # if monitor == 0 and status not in ['starting...', 'stopping...', 'restarting...', 'disable monitoring...', 'enable monitoring...']:
-        # return "Not monitored"
+    # return "Not monitored"
     return status
+
 
 @register.filter
 def status_class(status, monitor):
     # has to be first
     # if monitor == 0 and status not in ['starting...', 'stopping...', 'restarting...', 'disable monitoring...', 'enable monitoring...']:
-        # return 'blue'
+    # return 'blue'
     if status == 'running':
         return 'green'
     if status in ['starting...', 'stopping...', 'restarting...']:
@@ -61,11 +65,13 @@ def status_class(status, monitor):
     # else return error color
     return 'red'
 
+
 @register.filter
 def in_MB(value):
     if not isinstance(value, (int, str)):
         return ""
     return str(round(float(value)/1.e3, 1))+" MB"
+
 
 @register.filter
 def in_GB(value):
@@ -73,9 +79,9 @@ def in_GB(value):
         return ""
     return str(round(float(value)/1.e6, 1))+" GB"
 
+
 @register.filter
 def percent(value):
     if not isinstance(value, (float, str)):
         return ""
     return str(round(value, 1))+"%"
-
